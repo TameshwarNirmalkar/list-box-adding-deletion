cb349a7d69430dd02beab39f652f872ae74b151b
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2025-05-24 18:23:51 +0530

    fix the ui issues

--- src/components/AddList.tsx
+++ src/components/AddList.tsx
@@ -29,23 +29,39 @@ const AddListComponent = ({ titleList, onItemChange, onItemDelete, selectedList 
   );
 
   return (
-    <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-      {titleList.map((title: TitleI) => {
-        return (
-          <li key={title.id} className="flex justify-between hover:bg-gray-600 hover:text-white-700 w-full px-4 py-2 border-gray-200 dark:border-gray-600">
-            <span className={`text-sm cursor-pointer ${selectedList.includes(title.id) ? "text-red-500" : ""}`} onClick={() => onItemChangeHandler(title)}>
-              {title.label}
-            </span>
-            <span
-              className="cursor-pointer rounded-full border-solid border border-red-500 bg-red-800 flex w-5 justify-center text-sm"
-              onClick={() => onItemDeleteHandler(title)}
-            >
-              D
-            </span>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <div className=" bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold flex justify-between">
+            <span>Event Name</span>
+            <span>{selectedList.length}</span>
+          </div>
+          <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            {titleList.map((title: TitleI) => {
+              return (
+                <li
+                  key={title.id}
+                  className="flex justify-between hover:bg-gray-600 rounded hover:text-white-700 w-full px-4 py-2 border-gray-200 dark:border-gray-600"
+                >
+                  <span
+                    className={`text-sm cursor-pointer ${selectedList.includes(title.id) ? "text-red-500" : ""}`}
+                    onClick={() => onItemChangeHandler(title)}
+                  >
+                    {title.label}
+                  </span>
+                  <span
+                    className="cursor-pointer rounded-full border-solid border border-red-500 bg-red-800 flex w-5 justify-center text-sm"
+                    onClick={() => onItemDeleteHandler(title)}
+                  >
+                    D
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
